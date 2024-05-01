1a6c700e06c199bb9613582e4d8b78bd85a2713b
Arauz <86898345+kjarj54@users.noreply.github.com>
2024-05-01 17:53:15 -0600

    Aula 2 y 3

--- screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,9 @@
 package com.aluracursos.screenmatch;
 
+import com.aluracursos.screenmatch.model.DatosSerie;
+import com.aluracursos.screenmatch.principal.Principal;
+import com.aluracursos.screenmatch.service.ConsumoAPI;
+import com.aluracursos.screenmatch.service.ConvierteDatos;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,12 +11,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
 
+//	public static void main(String[] args) {
+//		SpringApplication.run(ScreenmatchApplication.class, args);
+//	}
+//
+//	@Override
+//	public void run(String... args) throws Exception {
+//		System.out.println("Hello World pero desde spring boot!");
+//		var consumoApi = new ConsumoAPI();
+//		var json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&&apikey=d8a223f3");
+//		//json = consumoApi.obtenerDatos("https://coffee.alexflipnote.dev/random.json");
+//		System.out.println(json);
+//
+//		ConvierteDatos conversor = new ConvierteDatos();
+//		DatosSerie datos = conversor.obtenerDatos(json, DatosSerie.class);
+//		System.out.println(datos);
+//	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hello World pero desde spring boot!");
+		Principal principal = new Principal();
+		principal.muestraElMenu();
 	}
 }
